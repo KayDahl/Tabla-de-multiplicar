fdde618ddbdc3852f99fbbb92c22da53c63141a2
BLP <benjamin.llanos730@gmail.com>
2024-09-10 15:35:27 -0300

    Solicita al usuario ingresar el numero

--- 10-9-2024.cpp
+++ 10-9-2024.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main (){
 
-int numero = 5;
+
+int numero
+cout<<"ingrese un numero"<<endl;
+cin>>numero;
+
 for (int i = 1; i <= 10; i++) {
     cout<<numero<<" x "<< i << " = " << numero*i<< endl;
 }
