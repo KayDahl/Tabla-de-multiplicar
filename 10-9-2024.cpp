b3c70608bc084346ab03b0474c7022259943364a
BLP <benjamin.llanos730@gmail.com>
2024-09-10 15:41:34 -0300

    Comprueva que el numero ingresado no sea menor a 0

--- 10-9-2024.cpp
+++ 10-9-2024.cpp
@@ -10,9 +10,16 @@ int numero
 cout<<"ingrese un numero"<<endl;
 cin>>numero;
 
+
+if (numero < 0){
+    cout<<"ingrese un numero positivo"<< endl;
+}else {
 for (int i = 1; i <= 10; i++) {
     cout<<numero<<" x "<< i << " = " << numero*i<< endl;
 }
+}
+
+
 
 
 return 0;
